db4c23edf3a3aa3a3e6ccbd5db8566312a9bd96e
Marc <marcvidalma@gmail.com>
2022-05-01 03:36:46 +0200

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import Login from './Login'
-import Login2 from './Login2'
 function App () {
-  const isLoggedIn = true
-  if (isLoggedIn) {
-    return <Login />
-  }
-  return <Login2 />
+  return <Login />
 }
 
 export default App
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,14 +1,13 @@
 import { Form, Input, Button, Checkbox } from 'antd'
+import { UserOutlined, LockOutlined } from '@ant-design/icons'
 import 'antd/dist/antd.css'
 import './Login.css'
-function Login () {
+
+const Login = () => {
   const onFinish = (values) => {
-    console.log('Success:', values)
+    console.log('Received values of form: ', values)
   }
 
-  const onFinishFailed = (errorInfo) => {
-    console.log('Failed:', errorInfo)
-  }
   return (
     <>
       <div className='container-general'>
@@ -17,65 +16,52 @@ function Login () {
         </div>
         <div className='form-container'>
           <Form
-            name='basic'
-            labelCol={{
-              span: 8
-            }}
-            wrapperCol={{
-              span: 16
-            }}
+            name='normal_login'
+            className='login-form'
             initialValues={{
               remember: true
             }}
             onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete='off'
           >
             <Form.Item
-              label='Username'
               name='username'
               rules={[
                 {
                   required: true,
-                  message: 'Please input your username!'
+                  message: 'Please input your Username!'
                 }
               ]}
             >
-              <Input />
+              <Input prefix={<UserOutlined className='site-form-item-icon' />} placeholder='Username' />
             </Form.Item>
-
             <Form.Item
-              label='Password'
               name='password'
               rules={[
                 {
                   required: true,
-                  message: 'Please input your password!'
+                  message: 'Please input your Password!'
                 }
               ]}
             >
-              <Input.Password />
+              <Input
+                prefix={<LockOutlined className='site-form-item-icon' />}
+                type='password'
+                placeholder='Password'
+              />
+            </Form.Item>
+            <Form.Item>
+              <Form.Item name='remember' valuePropName='checked' noStyle>
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+
+              <a className='login-form-forgot' href=''>
+                Forgot password
+              </a>
             </Form.Item>
 
-            <Form.Item
-              name='remember'
-              valuePropName='checked'
-              wrapperCol={{
-                offset: 8,
-                span: 16
-              }}
-            >
-              <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-
-            <Form.Item
-              wrapperCol={{
-                offset: 8,
-                span: 16
-              }}
-            >
-              <Button type='primary' htmlType='submit'>
-                Submit
+            <Form.Item>
+              <Button type='primary' htmlType='submit' className='login-form-button'>
+                Log in
               </Button>
             </Form.Item>
           </Form>
@@ -84,5 +70,4 @@ function Login () {
     </>
   )
 }
-
-export default Login
+export default () => <Login />
